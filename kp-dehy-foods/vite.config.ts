507585388d578785f8669d33ee07b088bbabfa1f
Daksh Padiya <daksh.padiya@sumasoft.net>
2025-07-25 12:38:02 +0530

    changed in vite config for renaming outDir and base dir

--- kp-dehy-foods/vite.config.ts
+++ kp-dehy-foods/vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/kp-dehy-foods/',
   plugins: [react()],
+  build: {
+    outDir: 'build'
+  }
 })
